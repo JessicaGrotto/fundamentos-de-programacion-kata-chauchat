7456d3ff04edba37969c422efb5254269622687a
JessicaGrotto <jessicalgrotto@gmail.com>
2024-08-27 19:38:40 +0200

    primera tentativa

---
+++ script.js
@@ -0,0 +1,14 @@
+const cargador = ["pium!","pium!","pium!","pium!","pium!","pium!","pium!"];
+
+function chauchat(cargador){
+    for (let i = 0; i < cargador.length; i++){
+     if(Math.random()<0.8){ //80% de probabilidade de bloqueo 
+            return console.log("Illo, me he quedao pillá!")
+        } 
+    for(let j = 0; j<3; j++){
+        return console.log("pium!");
+    } return console.log("")
+    }  
+};
+
+chauchat(cargador);
